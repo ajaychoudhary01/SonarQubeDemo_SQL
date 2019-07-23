1fc57af0b0307d7a7914c57b77e7a8e1e2cf59a0
Ajay Choudhary <ajaychoudhary091989@gmail.com>
2019-07-23 16:15:43 +0100

    Update and rename dbo.AspNetUsers.Table.sql to dbo.User.Table.sql

---
+++ dbo.User.Table.sql
@@ -0,0 +1,22 @@
+﻿USE [SomeSQLDB]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 7/21/2019 5:49:58 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[Id] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](256) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[UserName] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.AspNetUsers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+SET ANSI_PADDING ON
+
